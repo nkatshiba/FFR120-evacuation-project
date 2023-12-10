f6bd27ff2e71a7bb2ec727e6dae0b4405adaccde
nkatshiba <nkatshiba@proton.me>
2023-12-10 19:08:07 +0100

    feat(blobs): disorientation
    emil code inserted

--- src/blob.py
+++ src/blob.py
@@ -21,6 +21,19 @@ class Blob:
         else:
             return 4
 
+    def get_second_closest_exit(self, exit_points):
+        distances = []
+        for point in exit_points:
+            distances.append(np.sqrt((self.x - point[0]) ** 2 + (self.y - point[1]) ** 2))
+
+        if len(distances) >= 2:
+            sorted_indices = np.argsort(distances)
+            second_closest_index = sorted_indices[1]
+            second_closest_exit = exit_points[second_closest_index]
+            return second_closest_exit
+        else:
+            return None
+
     def check_proximity(self, blobs, threshold, min_velocity):
         close_blobs = 0
         for other_blob in blobs:
@@ -29,12 +42,14 @@ class Blob:
                 if distance < threshold:
                     close_blobs += 1
         if close_blobs >= 2:
-            self.velocity /= 2
+            self.velocity /= 1.1
             self.velocity = np.maximum(self.velocity, min_velocity)
 
-    def update(self, exit_points, alarm_on, stepsize, eta, D, blobs, threshold, min_velocity):
+    def update(self, exit_points, alarm_on, stepsize, eta, D, blobs, threshold, min_velocity, max_velocity, turn_around_steps):
         self.check_proximity(blobs, threshold, min_velocity)
+
         if alarm_on:
+
             quadrant = self.get_quadrant(D)
             if quadrant == 1:
                 preferred_exit = exit_points[0]
@@ -46,9 +61,27 @@ class Blob:
                 preferred_exit = exit_points[1]
 
             exit_direction = np.arctan2(preferred_exit[1] - self.y, preferred_exit[0] - self.x)
+
+            if np.linalg.norm(self.velocity) > max_velocity:
+                second_closest_exit = self.get_second_closest_exit(exit_points)
+                # If velocity exceeds the threshold, move towards the second closest exit point for a few steps.
+                if hasattr(self, 'turn_around_count') and self.turn_around_count > 0:
+                    # Continue moving towards the second closest exit.
+                    exit_direction = np.arctan2(second_closest_exit[1] - self.y, second_closest_exit[0] - self.x)
+                    self.angle = 0.5 * self.angle + 0.5 * exit_direction
+                    #noise = (-eta / 2 + np.random.rand(1) * eta / 2)
+                    self.angle = self.angle #+ noise.item()
+                    #reduce speed so that it doesn't keep going in the wrong direction
+                    v = self.velocity * 0.5 * np.array([np.cos(self.angle), np.sin(self.angle)])
+                    self.turn_around_count -= 1
+                else:
+                    #
+                    self.turn_around_count = turn_around_steps
+                    exit_direction = np.arctan2(preferred_exit[1] - self.y, preferred_exit[0] - self.x)
+
             self.angle = 0.5 * self.angle + 0.5 * exit_direction
-            noise = (-eta / 2 + np.random.rand(1) * eta / 2)
-            self.angle = self.angle + noise.item()
+            #noise = (-eta / 2 + np.random.rand(1) * eta / 2)
+            self.angle = self.angle #+ noise.item()
             v = self.velocity * np.array([np.cos(self.angle), np.sin(self.angle)])
         else:
             v = self.velocity
--- src/experiment.py
+++ src/experiment.py
@@ -14,8 +14,8 @@ def experiment(N, T, R, D, eta, stepsize, threshold, min_velocity):
 
     exit_point1 = np.array([D/2, D])  # TOP
     exit_point2 = np.array([D, D/2])  # RIGHT
-    exit_point3 = np.array([0, D/2])  # LEFT
-    exit_point4 = np.array([D/2, 0])  # BOT
+    check_point1 = np.array([0, D/2])  # LEFT
+    check_point2 = np.array([D/2, 0])  # BOT
 
     exit_counter = 0
     exited_blobs = set()
@@ -28,21 +28,23 @@ def experiment(N, T, R, D, eta, stepsize, threshold, min_velocity):
             if k == 0:
                 k = 1
                 for blob in blobs:
-                    blob.velocity = 0.08 * np.random.normal(eta * 5, 0.2)
+                    blob.velocity = 0.1 * np.random.normal(eta * 5, 0.2)
 
         for blob in blobs:
-            # blob.update([exit_point, exit_point2], alarm_on, stepsize, eta, D, blobs, threshold=1, min_velocity=0.01)
-
-            blob.update([exit_point1, exit_point2, exit_point3, exit_point4], alarm_on, stepsize, eta, D, blobs, threshold=1, min_velocity=0.01)
+            # blob.update([exit_point1, exit_point2, check_point1, check_point2], alarm_on, stepsize, eta, D, blobs, threshold=1, min_velocity=0.01)
+            blob.update([exit_point1, exit_point2, check_point1, check_point2], alarm_on, stepsize,
+                        eta, D, blobs, threshold=1, min_velocity=0.01, max_velocity=0.05, turn_around_steps=20)
 
         ax.clear()
         setup_plot(fig, ax, D)  # Reset the plot using the existing ax object
 
         for blob in blobs:
             if blob in exited_blobs:
-                ax.plot(blob.x, blob.y, '.', color='#808080')  # Exited blobs in gray
+                # Exited blobs in gray
+                ax.plot(blob.x, blob.y, '.', color='#808080')
             else:
-                ax.plot(blob.x, blob.y, '.', color='#0000FF')  # Moving blobs in blue
+                # Moving blobs in blue
+                ax.plot(blob.x, blob.y, '.', color='#0000FF')
 
         for blob in blobs:
             if blob not in exited_blobs and (np.linalg.norm(np.array([blob.x, blob.y]) - exit_point1) < R or np.linalg.norm(np.array([blob.x, blob.y]) - exit_point2) < R):
@@ -54,26 +56,32 @@ def experiment(N, T, R, D, eta, stepsize, threshold, min_velocity):
 
         exit_col1 = '#ff003d'
         exit_col2 = '#ff003d'
-        exit_col3 = '#69c3c3'
-        exit_col4 = '#ffa0cd'
+        cp_col1 = '#69c3c3'
+        cp_col2 = '#ffa0cd'
+
         exit_count_col = '#74e3b1'
         time_col = '#D3D3D3'
         alarm_col = '#FFFF00'
 
         ax.scatter(*exit_point1, color=exit_col1, s=200)
-        ax.text(*exit_point1, 'Exit 1', ha='center', va='bottom', color=exit_col1)
+        ax.text(*exit_point1, 'Exit 1', ha='center',
+                va='bottom', color=exit_col1)
         ax.scatter(*exit_point2, color=exit_col2, s=200)
-        ax.text(*exit_point2, 'Exit 2', ha='left', va='center', color=exit_col2)
-        ax.scatter(*exit_point3, color=exit_col3, s=200)
-        ax.text(*exit_point3, 'CP 1', ha='right', va='center', color=exit_col3)
-        ax.scatter(*exit_point4, color=exit_col4, s=200)
-        ax.text(*exit_point4, 'CP 2', ha='center', va='top', color=exit_col4)
+        ax.text(*exit_point2, 'Exit 2', ha='left',
+                va='center', color=exit_col2)
+        ax.scatter(*check_point1, color=cp_col1, s=200)
+        ax.text(*check_point1, 'CP 1', ha='right', va='center', color=cp_col1)
+        ax.scatter(*check_point2, color=cp_col2, s=200)
+        ax.text(*check_point2, 'CP 2', ha='center', va='top', color=cp_col2)
 
-        ax.text(0 + 0.2, D - 0.7, 'Exit counter: {}'.format(exit_counter), ha='left', va='top', color=exit_count_col, weight='bold', fontsize=10)
-        ax.text(0 + 0.2, D - 0.2, f'Time step: {t}', ha='left', va='top', color=time_col, weight='bold', fontsize=10)
+        ax.text(0 + 0.2, D - 0.7, 'Exit counter: {}'.format(exit_counter),
+                ha='left', va='top', color=exit_count_col, weight='bold', fontsize=10)
+        ax.text(0 + 0.2, D - 0.2, f'Time step: {t}', ha='left',
+                va='top', color=time_col, weight='bold', fontsize=10)
 
         if alarm_on:
-            ax.text(D/2, 0.7, 'Alarm ON', ha='center', va='bottom', color=alarm_col, fontsize=14)
+            ax.text(D/2, 0.7, 'Alarm ON', ha='center',
+                    va='bottom', color=alarm_col, fontsize=14)
 
         plt.draw()
         plt.pause(0.01)
@@ -87,4 +95,3 @@ def experiment(N, T, R, D, eta, stepsize, threshold, min_velocity):
         print(f"Escape time: {escape_time} time steps")
     else:
         print("Simulation ended without all individuals escaping.")
-
